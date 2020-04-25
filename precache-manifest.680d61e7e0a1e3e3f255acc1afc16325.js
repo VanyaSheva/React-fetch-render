769b5ae1e7fbae8c4ee3e5f86fe1807f7d5ce231
Vanya Shevchuk <aggressor2000@gmail.com>
2020-04-25 20:42:57 +0300

    Updates

---
+++ precache-manifest.680d61e7e0a1e3e3f255acc1afc16325.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "497758290fd407e8764a88f9078ebb91",
+    "url": "/React-fetch-render/index.html"
+  },
+  {
+    "revision": "04ab09a12b788f36cd07",
+    "url": "/React-fetch-render/static/css/main.f0ed4d97.chunk.css"
+  },
+  {
+    "revision": "4cd74ae875f8e7a25f37",
+    "url": "/React-fetch-render/static/js/2.b8f89381.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-fetch-render/static/js/2.b8f89381.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "04ab09a12b788f36cd07",
+    "url": "/React-fetch-render/static/js/main.0341f04b.chunk.js"
+  },
+  {
+    "revision": "419c9ebf9ea7563548e8",
+    "url": "/React-fetch-render/static/js/runtime-main.7f87b9f3.js"
+  }
+]);
